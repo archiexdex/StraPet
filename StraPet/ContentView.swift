219928b23cf0c5fab82789627a22942ee7135351
archiexdex <rc840327@gmail.com>
2020-12-03 16:37:31 +0800

    Initial Commit

---
+++ StraPet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StraPet
+//
+//  Created by Archie Yang on 2020/12/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
